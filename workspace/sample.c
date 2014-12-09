79ec0695ae9e453f96ca531f1be2b494e57c5c2f
ritesh2 <riteshvelaga@gmail.com>
2014-12-09 11:16:47 +0530

    Compressed

--- workspace/sample.c
+++ workspace/sample.c
@@ -1,15 +1,6 @@
-int i;
-
-
 #include<avr/io.h>
 #include<util/delay.h>
 #include<avr/leds.h>
 void main(){
-  while (1) {
-    for (i = 1; i <= 5; i++) {
-      led_on(i);
-      led_off(i - (1));
-      _delay_ms(1000);
-    }
-  }
+  led_on(1);
 }
